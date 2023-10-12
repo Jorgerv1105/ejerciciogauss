13db7d46da0b361306c6b4dd60ad69029946e0a2
Laboratorio <jorge.ramos.vasquez@udla.edu.ec>
2023-10-12 16:32:10 -0500

    final

--- ejerciciogauss1.c
+++ ejerciciogauss1.c
@@ -1,9 +1,18 @@
 #include <stdio.h> 
-int main(){ 
+int main(void) { 
     int n, suma; 
-    printf("Ingrese un numero: "); 
-scanf("%i",&n);
-suma=(n*(n+1))/2;
-printf("La suma de los primeros numeros %i numeros naturales es : %i\n", n, suma );
+    printf("Ingrese un numero natural: \n"); 
+    scanf("%i", &n);
+    if (n<=0){
+    while(n<=0)
+    {
+        printf("El numero ingresado no es un entero positivo vuelva a intentarlo\n")
+        scanf("%i", &n);
+
+    }
+    }
+    else
+    suma=(n*(n+1))/2;
+    printf("La suma de los primeros numeros %i numeros naturales es : %i\n", n, suma );
 return 0;
 }
