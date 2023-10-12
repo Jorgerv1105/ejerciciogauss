729b4bcbaa7a5b2f352c2e161dd9e7c16e8dc518
Laboratorio <jorge.ramos.vasquez@udla.edu.ec>
2023-10-12 16:19:28 -0500

    salida

--- ejerciciogauss1.c
+++ ejerciciogauss1.c
@@ -4,5 +4,6 @@ int main(){
     printf("Ingrese un numero: "); 
 scanf("%i",&n);
 suma=(n*(n+1))/2;
+printf("La suma de los primeros numeros %i numeros naturales es : %i\n", n, suma );
 return 0;
 }
